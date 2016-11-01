d05131287f0548ac84160965c5d206cc3110d7b2
Rashmi <geminirashmi31@gmail.com>
2016-11-01 02:25:50 -0700

    expand and collapse navigation menu working for desktop version.

--- js/script.js
+++ js/script.js
@@ -5,3 +5,30 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.height = "0%";
 }
+
+jQuery(document).ready(function() {
+	/* scroll setting*/
+	jQuery(window).scroll(function(){
+		if(jQuery(document).scrollTop() > 200 ){
+			jQuery(".site-header").css({"width": "93px"});
+		}
+		else {
+			jQuery(".site-header").css({"width": "100%"});
+		}
+	});
+	
+	/* mouse over*/
+    jQuery(".site-header").hover(
+		function(){ 
+		  if(jQuery(document).scrollTop() > 200 ){
+		  jQuery(this).stop().animate({"width": "100%"}, 'fast');} 
+		}, 
+		function(){
+			if(jQuery(document).scrollTop() > 200 ){
+			jQuery(this).stop().animate({"width": "93px"}, 'fast');} 
+		}
+			
+	);
+	
+});
+	
